fdf29e22b65c616e9f6e949427d63573d1d870c6
lim byeong cheol <lim6112j@gmail.com>
2020-05-29 14:44:08 +0900

    accum

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,17 @@
 f :: Int -> Int -> Int
 f x y = x*x + y*y
 main = print(even 2)
+-- version 1
+evenSum :: [Integer] -> Integer
+
+evenSum l = accumSum 0 l
+
+accumSum n l = if l == []
+                  then n
+                  else let x = head l
+                           xs = tail l
+                       in if even x
+                              then accumSum (n+x) xs
+                              else accumSum n xs
+
+evenSum [1..5]
