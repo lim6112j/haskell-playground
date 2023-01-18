6da2acce61e285818ed6c229abf06a07b4c88ac3
ben lim <lim6112j@gmail.com>
2023-01-18 19:08:40 +0900

    more monad reader example

--- simplereader/simplereader.hs
+++ simplereader/simplereader.hs
@@ -18,7 +18,28 @@ lookupVar name bindings = maybe 0 id (Map.lookup name bindings)
 sampleBindings :: Bindings
 sampleBindings = Map.fromList [("count", 3), ("1", 1), ("b", 2)]
 
+-- example 2
+
+calculateContentLen :: Reader String Int
+calculateContentLen = do
+  content <- ask
+  return (length content)
+
+calculateModifiedContentLen :: Reader String Int
+calculateModifiedContentLen = local ("prefix" ++) calculateContentLen
+
+printReaderContent :: ReaderT String IO ()
+printReaderContent = do
+  content <- ask
+  liftIO $ putStrLn ("The Reader Content: " ++ content)
+
 main :: IO ()
 main = do
   putStr $ "Count is correct for bindings " ++ (show sampleBindings) ++ ": "
   putStrLn $ show (isCountCorrect sampleBindings)
+  let s = "12345"
+  let modifiedLen = runReader calculateModifiedContentLen s
+  let len = runReader calculateContentLen s
+  putStrLn $ "Modified 's' length: " ++ (show modifiedLen)
+  putStrLn $ "Original 's' length: " ++ (show len)
+  runReaderT printReaderContent "Some Content"
