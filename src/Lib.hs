92b082e0dfc2b9ab4fb4218dd0b4a7a1dcd08261
ben lim <lim6112j@gmail.com>
2023-01-18 11:11:35 +0900

    evenNum

--- src/Lib.hs
+++ src/Lib.hs
@@ -86,5 +86,5 @@ import Data.List (foldl')
 sum' :: (Num a) => [a] -> a
 sum' = foldl' (+) 0
 evenSum :: Integral a => [a] -> a
-evenSum = sum' . (filter even)
+evenSum = sum' . filter even
 main = print(evenSum [1..5])
