85b79e2f80a40089b336ec48549439a4bfd2f980
lim6112j <lim6112j@gmail.com>
2023-01-20 02:11:41 +0900

    Parser but output not working

--- liftio/liftio.hs
+++ liftio/liftio.hs
@@ -5,5 +5,19 @@ printState :: Show s => StateT s IO ()
 printState = do
   s <- get -- get state
   lift $ print s -- IO () == value
+
+-- parser with state
+type Parser = StateT String [] -- StateT s m 
+runParser :: Parser a -> String -> [a]
+runParser p s = [x | (x, "") <- runStateT p s]
+
+item :: Parser Char
+item = do
+  c:cs <- get
+  put cs
+  return c
+  
 main :: IO ()
-main = evalStateT printState "hello" -- "hello" state, evalStateT returns value == IO ()
+main = do 
+  evalStateT printState "hello" -- "hello" state, evalStateT returns value == IO ()
+  print $ runParser item "good"
