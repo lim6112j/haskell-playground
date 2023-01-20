f406b8a97e75334950f3f9bac0ed01dc938d7fc4
lim6112j <lim6112j@gmail.com>
2023-01-20 22:28:33 +0900

    simplified arrow

--- arrow/arrow.hs
+++ arrow/arrow.hs
@@ -3,8 +3,13 @@ addA :: Arrow m => m a Int -> m a Int -> m a Int
 addA f g =
   arr (\x -> (x, x)) >>>
   first f >>> arr(\(y, x) -> (x,y)) >>>
-  first g >>> arr(\(y, z) -> y + z)
+  --first g >>> arr(\(y, z) -> y + z)
+  first g >>> arr(uncurry(+))
+addA' :: Arrow m => m a Int -> m a Int -> m a Int
+addA' f g = f &&& g >>> arr(uncurry(+))
+
 main :: IO ()
 main = do
   print $ addA (* 2) (^ 4) 2
+  print $ addA' (*4) (^2) 3
 
