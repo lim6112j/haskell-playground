81e0df617a250707857c202db5ae2ef0f5ac254a
lim6112j <lim6112j@gmail.com>
2023-09-02 05:12:13 +0900

    kind for instance make

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE InstanceSigs#-}
 module Main where
 
-import Lib
+-- import Lib
 -- import IOLib
+class Tofu t where
+  tofu :: j a -> t a j
+data Frank a b = Frank {frankField::b a} deriving (Show)
+
+instance Tofu Frank where
+  tofu x = Frank x
+data Barry t k p = Barry{yabba::p, papa::t k} deriving (Show)
+-- check kind for understanding below code
+instance Functor (Barry a b) where
+  fmap :: (x -> y) -> Barry c d x -> Barry c d y
+  fmap f (Barry {yabba = x, papa = y}) = Barry{ yabba = f x, papa = y }
 main :: IO()
-main = print(evenSum [1..5])
+main = print $ (\x -> x * 3) <$> Barry {yabba=2, papa = Just 2}
