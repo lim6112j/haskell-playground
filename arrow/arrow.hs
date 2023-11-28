fab3d2dd2e8ad60a52b73daab89bee96296e4369
ben lim <lim6112j@gmail.com>
2023-11-28 16:13:27 +0900

    more arrow notation

--- arrow/arrow.hs
+++ arrow/arrow.hs
@@ -1,15 +1,28 @@
-import Control.Arrow 
+{-# LANGUAGE Arrows #-}
+
+import Control.Arrow
+
 addA :: Arrow m => m a Int -> m a Int -> m a Int
 addA f g =
-  arr (\x -> (x, x)) >>>
-  first f >>> arr(\(y, x) -> (x,y)) >>>
-  --first g >>> arr(\(y, z) -> y + z)
-  first g >>> arr(uncurry(+))
+  arr (\x -> (x, x))
+    >>> first f
+    >>> arr (\(y, x) -> (x, y))
+    >>>
+    -- first g >>> arr(\(y, z) -> y + z)
+    first g
+    >>> arr (uncurry (+))
+
 addA' :: Arrow m => m a Int -> m a Int -> m a Int
-addA' f g = f &&& g >>> arr(uncurry(+))
+addA' f g = f &&& g >>> arr (uncurry (+))
+
+addA'' :: Arrow m => m a Int -> m a Int -> m a Int
+addA'' f g = proc x -> do
+  y <- f -< x
+  z <- g -< x
+  returnA -< y + z
 
 main :: IO ()
 main = do
   print $ addA (* 2) (^ 4) 2
-  print $ addA' (*4) (^2) 3
-
+  print $ addA' (* 4) (^ 2) 3
+  print $ addA'' (* 4) (^ 2) 3
