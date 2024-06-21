97b586ea9eca689c7c0957cdbf5e48b95ae057df
byeongcheollim <lim6112j@gmail.com>
2024-06-21 23:55:19 +0900

    haskell-in-depth du

---
+++ du/AppTypes.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE RecordWildCards #-}
+
+import Control.Monad.RWS.Lazy (RWST, evalRWST)
+
+data AppConfig = AppConfig
+  { basePath :: FilePath,
+    maxDepth :: Int,
+    extension :: Maybe String,
+    followSymlinks :: Bool
+  }
+
+data AppEnv = AppEnv
+  { cfg :: AppConfig,
+    path :: FilePath,
+    depth :: Int,
+    fileStatus :: FilePath
+  }
+
+intialEnv :: AppConfig -> AppEnv
+intialEnv config@AppConfig {..} =
+  AppEnv
+    { cfg = config,
+      path = basePath,
+      depth = 0,
+      fileStatus =
+        if followSymlinks
+          then getFileStatus
+          else getSymbolicLinkStatus
+    }
+
+getSymbolicLinkStatus :: FilePath
+getSymbolicLinkStatus = _
+
+getFileStatus :: FilePath
+getFileStatus = _
+type MyApp logEntry state = RWST AppEnv [logEntry] state IO
+runMyApp :: MyApp logEntry state a -> AppConfig -> state -> IO (a, [logEntry])
+runMyApp app config st = evalRWST app (initialEnv config) st
