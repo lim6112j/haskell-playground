02f926de25e41ecb1c71e4ff6e58f83298eff065
byeongcheollim <lim6112j@gmail.com>
2024-06-22 12:06:36 +0900

    du

---
+++ du/Utils.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+import AppTypes
+import Control.Monad.Trans (MonadIO (liftIO))
+import Control.Monad.Trans.RWS.Lazy (ask, asks, local)
+import Data.Foldable.Extra (traverse_)
+import System.Directory.Extra (listDirectory)
+import System.FilePath ((</>))
+import System.PosixCompat.Files (FileStatus)
+
+currentPathStatus :: MyApp l s FileStatus
+currentPathStatus = do
+  AppEnv {fileStatus, path} <- ask
+  liftIO $ fileStatus path
+
+traverseDirectoryWith :: MyApp le s () -> MyApp le s ()
+traverseDirectoryWith app = do
+  curPath <- asks path
+  content <- liftIO $ listDirectory curPath
+  traverse_ go content
+  where
+    go name = flip local app $
+      \env ->
+        env
+          { path = path env </> name,
+            depth = depth env + 1
+          }
